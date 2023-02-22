cab28466f30c76921264b23158c40cecceb0b9c1
Kwaiia01 <124386203+Kwaiia01@users.noreply.github.com>
2023-02-21 21:00:24 -0600

    Update CorporateBond.java
    Updated from the main EclipseIDE version

--- stock_Market/src/stock_Market/CorporateBond.java
+++ stock_Market/src/stock_Market/CorporateBond.java
@@ -2,11 +2,11 @@ package stock_Market;
 
 public class CorporateBond extends Bond 
 {
-
+	String name = "";
 	double faceValue, purchasePrice, defaultProbability, discountRate, couponRate; //PurchasePrice = price of bond during purchase
 	int compoundFreq, maturityTime, defaultPayout;		//compoundFreq = compounding frequency, maturityTime = time to maturity
 	
-	public CorporateBond() 
+	public CorporateBond(String n) 
 	{
 		compoundFreq = 1;
 		maturityTime = 3;
@@ -15,9 +15,10 @@ public class CorporateBond extends Bond
 		defaultPayout = 600;
 		defaultProbability = .10;
 		discountRate = 0.05;
+		name = n;
 	}
 	
-	public double bondValue() //aka bond Price
+	private double bondValue() //aka bond Price
 	{
 		double price = 0;
 		
@@ -29,18 +30,21 @@ public class CorporateBond extends Bond
 		return price;
 	}
 
-	public double yieldToMaturityPercent()
+	private double yieldToMaturity()
 	{
-		for(int i = 1; i<maturityTime; i++)
-		{
-			//https://www.omnicalculator.com/finance/bond-ytm#how-to-calculate-yield-to-maturity-the-bond-yield-to-maturity-formula
-		}
-		
-		return 0;
+		return (couponPayment(faceValue, couponRate, compoundFreq) +  ( (faceValue - bondValue() )/ maturityTime ) ) / ( ( faceValue + bondValue() ) / 2) * 100;
+	}
+	
+	public double getValue() {
+		return bondValue();
+	}
+	
+	public double getYTM(){
+		return yieldToMaturity();
 	}
 	
 	public String toString()
 	{
-		return String.format("The expected price of the bond is $%.2f \nThe rate of return of the bond is %.2f", bondValue(), yieldToMaturityPercent()*100) + "%";
+		return String.format("\n\n" + name + ":  \nThe expected price of the bond is $%.2f \nThe yield to maturity of the bond is %.2f", bondValue(), yieldToMaturity()) + "%";
 	}
 }
